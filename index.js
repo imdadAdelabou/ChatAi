daeac28a582f427cbe716ad43417ecafb1215388
imdad Adelabou <imdadadelabou@imdads-MacBook-Pro.local>
2024-10-12 10:58:38 +0200

    [FEAT] implement the semantic search

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 //prompt_token: is how many the prompt has
 //finish_reason: sometine it might respond and might not finish
+
+//https://scottmoss.notion.site/AI-App-Node-js-f9a372a138ef4241943b4fbb44bdc970
